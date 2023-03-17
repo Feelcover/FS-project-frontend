0d17df0bdaff4db57e5ed7462e9ec39fdfaa7056
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-03-17 17:20:43 +0300

    типизация корневого index

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
