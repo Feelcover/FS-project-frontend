75480a991f06280b843828e31eb2fd4959ac4755
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-03-17 17:37:57 +0300

    роутинг

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 import CssBaseline from "@mui/material/CssBaseline";
-import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+
+import "./index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,7 +16,9 @@ root.render(
   <React.StrictMode>
     <CssBaseline />
     <ThemeProvider theme={theme}>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
