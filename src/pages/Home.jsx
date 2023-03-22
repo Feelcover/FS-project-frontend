640311da8372f0b081f3429f984a015d36b91e36
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-03-22 23:02:42 +0300

    билдер и экшен для получения тегов

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,15 @@ import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchPosts } from '../redux/slices/post';
+import { fetchPosts, fetchTags } from '../redux/slices/post';
 
 export const Home = () => {
   const {posts, tags} = useSelector(state => state.postsReducer);
   const isLoading = posts.status === "loading";
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchPosts())
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, [])
   
   return (
@@ -33,13 +34,9 @@ export const Home = () => {
               id={item._id}
               title={item.title}
               imageUrl={item.imageUrl}
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
+              user={item.user}
               createdAt={item.createdAt}
-              viewsCount={150}
+              viewsCount={item.viewsCount}
               commentsCount={3}
               tags={item.tags}
               isEditable
