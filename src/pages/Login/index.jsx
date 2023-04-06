cd1a097951846e392cb6ea54e2c64b4c3a2dac32
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-06 15:57:35 +0300

    валидация и подключение формы авторизации

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,24 +3,51 @@ import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import styles from "./Login.module.scss";
+import { useForm } from "react-hook-form";
 
 export const Login = () => {
-  return (
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+  const onSubmit = (value) => {
+    console.log(value);
+  };
+   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          type = "email"
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register("email", { required: "Укажите почту" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          helperText={errors.password?.message}
+          error={Boolean(errors.password?.message)}
+          label="Пароль"
+          {...register("password", { required: "Укажите пароль" })}
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
