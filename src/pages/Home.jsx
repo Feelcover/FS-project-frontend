248344aa9fd90cfff1a95153a5c72ae9d57cf84f
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-07 15:18:27 +0300

    запрет на удаление и редактирование поста, если он не твой

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,32 @@
-import { useEffect } from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Grid from '@mui/material/Grid';
-import { Post } from '../components/Post';
-import { TagsBlock } from '../components/TagsBlock';
-import { CommentsBlock } from '../components/CommentsBlock';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchPosts, fetchTags } from '../redux/slices/post';
+import { useEffect } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Grid from "@mui/material/Grid";
+import { Post } from "../components/Post";
+import { TagsBlock } from "../components/TagsBlock";
+import { CommentsBlock } from "../components/CommentsBlock";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts, fetchTags } from "../redux/slices/post";
+import { isAuthSelector } from "../redux/slices/auth";
 
 export const Home = () => {
-  const { posts, tags } = useSelector(state => state.postsReducer);
+  const { posts, tags } = useSelector((state) => state.postsReducer);
+  const userData = useSelector((state) => state.authReducer.data);
   const isLoadingPosts = posts.status === "loading";
   const isLoadingTags = tags.status === "loading";
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchPosts());
     dispatch(fetchTags());
-  }, [])
+  }, []);
 
   return (
     <>
-      <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
+      <Tabs
+        style={{ marginBottom: 15 }}
+        value={0}
+        aria-label="basic tabs example"
+      >
         <Tab label="Новые" />
         <Tab label="Популярные" />
       </Tabs>
@@ -39,7 +45,7 @@ export const Home = () => {
                 viewsCount={item.viewsCount}
                 commentsCount={3}
                 tags={item.tags}
-                isEditable
+                isEditable = {userData && userData._id === item.user._id}
               />
             )
           )}
@@ -50,17 +56,17 @@ export const Home = () => {
             items={[
               {
                 user: {
-                  fullName: 'Александр Гончаров',
-                  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                  fullName: "Александр Гончаров",
+                  avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
                 },
-                text: 'Это братишка',
+                text: "Это братишка",
               },
               {
                 user: {
-                  fullName: 'Иван Иванов',
-                  avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                  fullName: "Иван Иванов",
+                  avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
                 },
-                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+                text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
               },
             ]}
             isLoading={false}
